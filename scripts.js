7406abd2a9c2b6047814661fa33db06f1b77e816
Randy Foo <randyfoo07@gmail.com>
2020-01-08 04:42:00 +1100

    Implement resize button

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
 const sketchPad = document.querySelector('.sketchPad');
 const resizeButton = document.querySelector('.left button');
-
+resizeButton.addEventListener('click', getSize);
 
 let size = 16;
 let squareDiv = [];
 
 setSize(size);
 
-// resizeButton.addEventListener('click', getSize());
-
-for (let i = 0; i < size * size; i++) {
-    squareDiv[i] = document.createElement('div');
-    squareDiv[i].classList.add('squareDiv');
-    sketchPad.appendChild(squareDiv[i]);
-}
 
 function setSize(num) {
     sketchPad.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     sketchPad.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        squareDiv[i] = document.createElement('div');
+        squareDiv[i].classList.add('squareDiv');
+        sketchPad.appendChild(squareDiv[i]);
+    }
 }
 
-// function getSize() {
-//     size = Number(prompt('What size to resize to (eg. type 16 for 16x16'));
-// }
+function getSize() {
+    size = Number(prompt('What size to resize to (eg. type 16 for 16x16'));
+    setSize(size);
+}
