295246e116719d2bb9f64db83e07b26d093f1dd7
Randy Foo <randyfoo07@gmail.com>
2020-01-09 01:32:30 +1100

    Complete resize function

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
 const sketchPad = document.querySelector('.sketchPad');
 const resizeButton = document.querySelector('.left button');
-resizeButton.addEventListener('click', getSize);
 
 let size = 16;
 let squareDiv = [];
 
-setSize(size);
+setSize(size, squareDiv);
 
+resizeButton.addEventListener('click', getSize);
+squareDiv.forEach(square => square.addEventListener('mouseover', colorSquare));
 
-function setSize(num) {
+function setSize(num, array) {
     sketchPad.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     sketchPad.style.gridTemplateRows = `repeat(${num}, 1fr)`;
-
-    for (let i = 0; i < size * size; i++) {
-        squareDiv[i] = document.createElement('div');
-        squareDiv[i].classList.add('squareDiv');
-        sketchPad.appendChild(squareDiv[i]);
+    for (let i = 0; i < num * num; i++) {
+        array[i] = document.createElement('div');
+        array[i].classList.add('squareDiv');
+        sketchPad.appendChild(array[i]);
     }
 }
 
 function getSize() {
-    size = Number(prompt('What size to resize to (eg. type 16 for 16x16'));
-    setSize(size);
+    size = Number(prompt('What size to resize to (default = 16 (16x16))'));
+    let squareDiv = [];
+    sketchPad.innerHTML = '';
+    setSize(size, squareDiv);
+    squareDiv.forEach(square => square.addEventListener('mouseover', colorSquare));
 }
+
+function colorSquare(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+//glear afer resizing
